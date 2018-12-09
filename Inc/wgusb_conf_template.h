73352f9091bd2c613b57505bd8c4de139b3d5daa
oclyke <oclyke@gmail.com>
2018-12-09 10:03:11 -0700

    Add multiple language ID support
    Also changed comments regarding number of endpoint definitions

--- Inc/wgusb_conf_template.h
+++ Inc/wgusb_conf_template.h
@@ -12,14 +12,16 @@ Aythor: Owen Lyke
 
 
 #define WGUSB_LPM_SUPPORT 	0		// Change to 1 if your driver supports link power management
-#define WGUSB_MAX_ENDPTS_IN		5	// The number of IN endpoints that the driver supports
-#define WGUSB_MAX_ENDPTS_OUT	5	// The number of OUT endpoints that the driver supports
+#define WGUSB_MAX_ENDPTS_IN		5	// The number of IN endpoints that the driver supports - not including ep0
+#define WGUSB_MAX_ENDPTS_OUT	5	// The number of OUT endpoints that the driver supports - not including ep0
 #define WGUSB_EP0_MPS 64			// The maximum packet size for the control (ep0) endpoint of the driver
 
 #define WGUSB_MAX_CONF 1		// This is the number of configurations that each device type contains
 #define WGUSB_MAX_INTFC 2		// This is the number of interfaces that each configuration type contains. 
 
 #define WGUSB_DEF_LANGID 0x0409
+#define WGUSB_NUM_LANGID 1			// This is the number of language IDs that are stored in the 0th string descriptor
+
 
 
 
