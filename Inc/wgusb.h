8141269958000a1bf2d0bcf8aea7de6e2ca04264
oclyke <oclyke@gmail.com>
2018-12-09 10:00:32 -0700

    Create wgusb.h
    This main header will list the end user serving API functions

---
+++ Inc/wgusb.h
@@ -0,0 +1,17 @@
+/*
+
+This is the main header file for Waveguide USB
+
+Author: Owen Lyke
+
+*/
+
+#ifndef __WGUSB_H_
+#define __WGUSB_H_
+
+#include "wgusb_def.h"	
+
+// API:
+
+
+#endif /* __WGUSB_H_ */
